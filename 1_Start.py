7e77a5d5db9a730d02a37154288f786f4182457f
COCOBLUE <cocoblue@kakao.com>
2021-02-27 15:04:06 +0900

    - Upload Prev Version Of this repo.

--- 1_Start.py
+++ 1_Start.py
@@ -1,59 +1,47 @@
+# discord 라이브러리 사용 선언
 import discord
-from discord.ext import commands
-
-# External File
-import load_json_variable as variable
-
-# Define the prefix. All command can call after prefix.
-prefix = "!"
-bot = commands.Bot(command_prefix=prefix)
 
 
-@bot.event
-async def on_ready():
-    """
-    This event runs when load the bot or re-load.
+class chatbot(discord.Client):
+    # 프로그램이 처음 실행되었을 때 초기 구성
+    async def on_ready(self):
+        # 상태 메시지 설정
+        # 종류는 3가지: Game, Streaming, Activity
+        game = discord.Game("내용")
 
-    :return: None
-    """
+        # 계정 상태를 변경한다.
+        # 온라인 상태, game 중으로 설정
+        await client.change_presence(status=discord.Status.online, activity=game)
 
-    # Define the activity 'Playing A Game' as 'comment'
-    game = discord.Game("comment")
-    await bot.change_presence(status=discord.Status.online, activity=game)
-    print("READY")
+        # 준비가 완료되면 콘솔 창에 "READY!"라고 표시
+        print("READY")
 
+    # 봇에 메시지가 오면 수행 될 액션
+    async def on_message(self, message):
+        # SENDER가 BOT일 경우 반응을 하지 않도록 한다.
+        # message.author.bot = message의 author가 bot이냐 아니냐를 질의하여 True or False로 표시
+        # 즉, if는 True일 때 : 밑에 들여쓰기 된 내용을 시행하므로, author가 Bot일 경우 아래의 코드를 실행
+        if message.author.bot:
+            # return None = 아무 것도 Return 하지 않음. 함수의 수행을 끝내는 역할을 함.
+            # 즉, return None이 실행된다면, 순차 지향인 코드에서는 아랫 줄의 코드를 실행하지 않게 됨.
+            return None
 
-@bot.event
-async def on_message(message):
-    """
-    This event runs when receive message. but It has no restriction of prefix.
-
-    :param message: discord.Message
-    :return: None
-    """
-
-    # When bot sent message, do nothing.
-    if message.author.bot:
-        return None
-
-    # Process Commands
-    await bot.process_commands(message)
-
-
-@bot.command(name="test")
-async def react_test(ctx):
-    """
-    This command runs when user call 'test' after prefix.
-
-    :param ctx: discord.context
-    :return: None
-    """
-
-    # Send a message to channel what user sent
-    await ctx.channel.send("testing!")
-    return None
+        # message.content = message의 내용
+        # == "!바보" = 왼쪽의 값이 "!바보"와 완벽 일치하면 True, 아니면 False를 리턴
+        # 즉, 메시지의 내용이 "!바보"라고 하면 : 밑에 들여쓰기 된 내용을 시행하게 됨.
+        if message.content == "!바보":
+            # 현재 채널을 받아옴
+            channel = message.channel
+            # 답변 내용 구성
+            msg = "너도 바보"
+            # msg에 지정된 내용대로 메시지를 전송
+            await channel.send(msg)
+            return None
 
 
-# Referencing token in Json file and run the bot.
-bot.run(variable.get_token())
-
+# 프로그램이 실행되면 제일 처음으로 실행되는 함수
+if __name__ == "__main__":
+    # 클래스 객체를 생성
+    client = chatbot()
+    # TOKEN 값을 통해 로그인하고 봇을 실행
+    client.run("Your Discord Bot Token")
--- Readme.md
+++ Readme.md
@@ -1,5 +1,7 @@
 Discord.py Example
 ======
+This is prev version of repo. It made by Discord Client.
+
 > Development Environment
 > ------
 > - Python 3.6  
@@ -9,12 +11,11 @@ Discord.py Example
 > ------
 > This repo is my blog post example for making Discord.py based Discord bot.  
 You can read blog post on URL below.  
-URL: https://blog.naver.com/6116949 (See the "Coding" Category (Only In Korean))  
-I try to write English explanation in the .py file.  
+URL: https://blog.naver.com/6116949 (See the "Coding" Category (Only In Korean))
 If you have any question or error, plz open the issue. then, I'll do my best to answer your issue.
 
-> Discord.py Example (한국어)
-> ========  
+> Korean Summary
+> ------
 > 이 저장소는 블로그에서 Discord.py를 기반으로 만든 Discord 봇 만드는 글 내용에서 사용한 소스코드를 저장한 곳입니다.  
 블로그의 글은 아래 링크에서 확인하실 수 있어요.  
 URL: https://blog.naver.com/6116949 ("Coding" 카테고리를 봐주세요.)  
